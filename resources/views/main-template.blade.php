8e738440fd0a755fcbf9c279d0d4f6dfbdb5f244
Afeique Sheikh <afeique@gmail.com>
2015-10-26 20:21:56 -0400

    die die die

--- resources/views/main-template.blade.php
+++ resources/views/main-template.blade.php
@@ -18,6 +18,7 @@
 
         <p>Hello there.</p>
 
+        <script src="vendor/html5shiv.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="vendor/jquery-1.11.3.min.js"><\/script>')</script>
 
