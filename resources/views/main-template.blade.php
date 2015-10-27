ce6f128cadcffdffa344dbe89b3c016357c79477
Afeique Sheikh <afeique@gmail.com>
2015-10-27 11:16:43 -0400

    I DON'T KNOW, OKAY

--- resources/views/main-template.blade.php
+++ resources/views/main-template.blade.php
@@ -8,19 +8,57 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <!-- Place favicon.ico in the root directory -->
+        <link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
+
+        <style type="text/css">
+            .main.menu {
+                border: none;
+                box-shadow: none;
+                font-size: 1em;
+            }
+            .main.menu .item {
+                padding: .25em;
+            }
+            .main.menu .header.item {
+                font-size: 1.2em;
+                margin-right: 1em;
+            }
+
+        </style>
     </head>
     <body>
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
+        <div class="ui text container">
+            <ul class="ui borderless main menu">
+                <li class="header item">
+                    acrossti.me
+                </li>
+                <li class="item">
+                    <a class="ui mini red button">
+                        feed
+                    </a>
+                </li>
+                <li class="item">
+                    <a class="ui mini basic violet button">
+                        journal
+                    </a>
+                </li>
+                <li class="item">
+                    <a class="ui mini basic blue button">
+                        friends
+                    </a>
+                </li>
 
-        <p>Hello there.</p>
+            </ul>
+        </div>
 
         <script src="vendor/html5shiv.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="vendor/jquery-1.11.3.min.js"><\/script>')</script>
+        <script src="semantic/semantic.min.js"></script>
 
         <?php /*
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
