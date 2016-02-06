02c5b9a5326293fa3e166e1fa0519f1018d14935
Afeique Sheikh <afeique@gmail.com>
2016-02-06 00:45:54 -0500

    test zurb build

--- resources/views/main-template.blade.php
+++ resources/views/main-template.blade.php
@@ -8,7 +8,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
 
         <style type="text/css">
             .main.menu {
